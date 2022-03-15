74b34845ef88ff83ef09611edf910fd3a8a1ac83
yamato3010 <yama3to10yama3@gmail.com>
2022-03-15 12:11:55 +0900

    不要なimportを削除

--- node/django_front/src/components/StaffManager.jsx
+++ node/django_front/src/components/StaffManager.jsx
@@ -15,8 +15,6 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import PropTypes from 'prop-types';
 
