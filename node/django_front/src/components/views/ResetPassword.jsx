448a594c232c9cba33a035d69f46500f070afe64
yamato3010 <yama3to10yama3@gmail.com>
2022-03-18 09:53:45 +0900

    パスワードが不一致の際に出現するアラートを追加、未使用のimport文を削除

--- node/django_front/src/components/views/ResetPassword.jsx
+++ node/django_front/src/components/views/ResetPassword.jsx
@@ -2,16 +2,14 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import Collapse from '@mui/material/Collapse';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import SimpleNavbar from '../SimpleNavbar'
-import NewAccountButton from './NewAccountButton';
+
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
@@ -28,20 +26,35 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function ResetPassword() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+
+    const [error, setError] = React.useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // eslint-disable-next-line no-console
+        console.log({
+            password: data.get('password'),
+            password2: data.get('password2'),
+        });
+        
+        if (data.get('password') === data.get('password2')){
+            //もしパスワードと再確認用のパスワードが一致したら
+            console.log("一致");
+            setError(false);
+        } else {
+            //もしパスワードと再確認用のパスワードが一致しなかったら
+            setError(true);
+            console.log("不一致");
+        }
+    };
 
   return (
     <ThemeProvider theme={theme}>
+    <Collapse in={error}>
+    <Alert severity="error">パスワードが一致しません</Alert>
+    </Collapse>
       <Container component="main" maxWidth="xs">
-
         <CssBaseline />
         <Box
           sx={{
@@ -60,13 +73,14 @@ export default function ResetPassword() {
               id="password"
               label="パスワード"
               name="password"
+              type="password"
               autoFocus
             />
             <TextField
               margin="normal"
               required
               fullWidth
-              name="password"
+              name="password2"
               label="パスワード(再確認)"
               type="password"
               id="password2"
