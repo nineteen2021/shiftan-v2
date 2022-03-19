dc8f5a67b87121850b5171941e24ebd28dfb755c
KOKI FURUYA <73323798+koki-fore@users.noreply.github.com>
2022-03-20 08:12:35 +0900

    Create logo (#38)
    * faviconとタイトルを変更
    
    * NavbarとSimpleNavbarでロゴタイトルをclickできるようにした
    
    * ロゴタイトルにclassを追加
    
    
    * ロゴのサイズを40pxに調節
    
    * react-router-domは入れていなくてエラーが出るのでコメントアウト
    
    * ロゴとファビコンの大きさを変更（ファビコンは完璧）
    
    * SimpleNavbarにもロゴのcssを適用
    
    * Navbarの方のタイトルの位置を修正

--- node/django_front/src/components/views/NewAccount.jsx
+++ node/django_front/src/components/views/NewAccount.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from 'react-router-dom';
+// import Link from 'react-router-dom';
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import { ButtonGroup } from '@mui/material';
