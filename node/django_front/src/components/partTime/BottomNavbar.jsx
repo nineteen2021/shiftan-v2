bc0e359e4575f57c251e60f31f9d72ac49095f75
yamato3010 <yama3to10yama3@gmail.com>
2022-04-06 09:43:59 +0900

    不要なimportを削除

--- node/django_front/src/components/partTime/BottomNavbar.jsx
+++ node/django_front/src/components/partTime/BottomNavbar.jsx
@@ -1,18 +1,10 @@
 import * as React from 'react';
 import '../../App.css'
 import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import SettingsIcon from '@mui/icons-material/Settings';
 import HomeIcon from '@mui/icons-material/Home';
-import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 export default function BottomNavbar() {
   const [value, setValue] = React.useState(0);
