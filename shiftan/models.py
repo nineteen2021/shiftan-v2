5a771efba37ce493b6a295cfb423c437f44974ea
koki-fore <furukunn@gmail.com>
2022-04-09 23:58:38 +0900

    足りないclassの追加とmax_lengthを小さくした。

--- shiftan/models.py
+++ shiftan/models.py
@@ -5,19 +5,24 @@ class Store(models.Model):
     store_name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     phone = models.IntegerField()
-    store_ID = models.SlugField(max_length=100)
+    store_ID = models.SlugField(max_length=50, unique=True)    
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=50)
+    color = models.SlugField(max_length=100)
 
 class Member(models.Model):
-    last_name = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    mail = models.EmailField(max_length=255)
-    user_ID = models.SlugField(max_length=100)
-    password = models.SlugField(max_length=100)
+    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    mail = models.EmailField(max_length=255, unique=True)
+    phone = models.IntegerField()
+    password = models.SlugField(max_length=50)
     store_FK = models.ForeignKey(Store, on_delete=models.DO_NOTHING)
+    group_FK = models.ForeignKey(Group, on_delete=models.DO_NOTHING)
     withdraw = models.BooleanField() #初期値False
 
 class Authority(models.Model):
-    authority_name = models.CharField(max_length=100)
+    authority_name = models.CharField(max_length=50)
 
 class Member_Authority(models.Model):
     member_FK = models.ForeignKey(Member, on_delete=models.DO_NOTHING)
@@ -36,6 +41,14 @@ class Work_Schedule(models.Model):
     create_time = models.DateTimeField(auto_now=True, auto_now_add=False)
     member_FK = models.ForeignKey(Member, on_delete=models.DO_NOTHING)
 
-class Group(models.Model):
-    group_name = models.CharField(max_length=100)
-    color = models.SlugField(max_length=100)
+class Shift_Range(models.Model):
+    shift_name = models.CharField(max_length=100)
+    start_date = models.DateField(auto_now=False, auto_now_add=False)
+    stop_date = models.DateField(auto_now=False, auto_now_add=False)
+    create_date = models.DateField(auto_now=True, auto_now_add=False)
+    deadline_date = models.DateField()
+
+class Schedule_Template(models.Model):
+    start_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    stop_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    
