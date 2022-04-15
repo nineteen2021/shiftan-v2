a3bf433222fac1b7175dbc13fe51d1c35e22032c
koki-fore <furukunn@gmail.com>
2022-04-15 18:53:44 +0900

    管理画面を少し修正

--- shiftan/admin.py
+++ shiftan/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.utils.translation import ugettext_lazy as _
-from .models import User
+from .models import User, Store, Group
 
 class MyUserChangeForm(UserChangeForm):
     class Meta:
@@ -12,12 +12,12 @@ class MyUserChangeForm(UserChangeForm):
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('email','username')
+        fields = ('email','first_name','last_name')
 
 class MyUserAdmin(UserAdmin):
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'username')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+        ('ユーザー情報', {'fields': ('email', 'password', 'last_name','first_name', 'phone')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_manager',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
@@ -29,11 +29,12 @@ class MyUserAdmin(UserAdmin):
     )
     form = MyUserChangeForm
     add_form = MyUserCreationForm
-    list_display = ('email', 'username', 'is_staff')
+    list_display = ('email', 'last_name','first_name', 'is_staff', 'is_manager')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('email', 'username')
+    search_fields = ('email', 'last_name','first_name')
     ordering = ('email',)
 
 admin.site.register(User, MyUserAdmin)
-
+admin.site.register(Store)
+admin.site.register(Group)
 # Register your models here.
