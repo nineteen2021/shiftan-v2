fac4440ebd58350a3223a7c2c7a98b95a6af224a
koki-fore <furukunn@gmail.com>
2022-05-07 18:24:38 +0900

    トークンの取得を完了

--- djangopj/settings.py
+++ djangopj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -75,9 +76,14 @@ WSGI_APPLICATION = 'djangopj.wsgi.application'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
 
+SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+}
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
