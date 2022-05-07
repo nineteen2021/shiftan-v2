927ecafdc3e9da693537d5a4b932c948584160c8
JinA293 <84577532+JinA293@users.noreply.github.com>
2022-05-07 17:33:24 +0900

    axiosとCORSを使ってReactにDBのデータを表示 (#148)
    *アルバイト用設定画面でaxiosとCORSを使った reactとDjangoの接続に成功
    
    * axiosのダウングレード
    
    * 店舗用アカウント設定画面でAxiosの設定
    
    * テスト用routeを削除

--- djangopj/settings.py
+++ djangopj/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000'
 ]
 
 ROOT_URLCONF = 'djangopj.urls'
