6e3f0884ff0b2e1c147450fdd59571d1b70d37b9
koki-fore <furukunn@gmail.com>
2022-05-12 17:21:01 +0900

    APIのJWT認証が完了

--- djangopj/settings.py
+++ djangopj/settings.py
@@ -74,11 +74,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangopj.wsgi.application'
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.AllowAny',
+#         'rest_framework.permissions.IsAuthenticated',
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     ],
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_simplejwt.authentication.JWTAuthentication',
+#     )
+# }
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
         'rest_framework.permissions.IsAuthenticated',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
