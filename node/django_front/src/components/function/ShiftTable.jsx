e2aee2c557756a099779b2858778117501fa2030
JinA293 <jin.codegym1217@gmail.com>
2022-06-11 14:20:23 +0900

    表に名前を表示できるようになった

--- node/django_front/src/components/function/ShiftTable.jsx
+++ node/django_front/src/components/function/ShiftTable.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,20 +7,38 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import axios from 'axios';
 
-function createData(name, first, second, third) {
-  return { name, first, second, third};
-}
+// function createData(name, first, second, third) {
+//   return { name, first, second, third};
+// }
 
-const rows = [
-  createData('テスト1', "15:00~20:00", "15:00~20:00", "15:00~20:00"),
-  // createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  // createData('Eclair', 262, 16.0, 24, 6.0),
-  // createData('Cupcake', 305, 3.7, 67, 4.3),
-  // createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+// const rows = [
+//   createData('テスト1', "15:00~20:00", "15:00~20:00", "15:00~20:00"),
+//   // createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+//   // createData('Eclair', 262, 16.0, 24, 6.0),
+//   // createData('Cupcake', 305, 3.7, 67, 4.3),
+//   // createData('Gingerbread', 356, 16.0, 49, 3.9),
+// ];
 
 export default function ShiftTable() {
+
+  const [users, setUsers] = useState(null)
+  const [tables, setTables] = useState(null)
+
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:8000/api-auth/users/',{
+        headers: {
+            'Authorization': `JWT ${localStorage.getItem('access')}`,
+        }
+    })
+    .then(res=>{setUsers(res.data);
+                console.log(res.data);})
+    .catch(err=>{console.log(err);});
+    }, []);
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }}>
@@ -32,15 +51,15 @@ export default function ShiftTable() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {users?.map((user) => (
             <TableRow
-              key={row.name}
+              // key={row.name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">{row.name}</TableCell>
-              <TableCell align="right">{row.first}</TableCell>
+              <TableCell component="th" scope="row">{user.first_name}</TableCell>
+              {/* <TableCell align="right">{row.first}</TableCell>
               <TableCell align="right">{row.second}</TableCell>
-              <TableCell align="right">{row.third}</TableCell>
+              <TableCell align="right">{row.third}</TableCell> */}
             </TableRow>
           ))}
         </TableBody>
