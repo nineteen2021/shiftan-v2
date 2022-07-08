010a3bc4148da4b5691cdcb163f31593a56fcc7f
koki-fore <furukunn@gmail.com>
2022-07-08 21:01:02 +0900

    コンソールに出す場所を変更

--- node/django_front/src/components/function/endPoint/Refresh.jsx
+++ node/django_front/src/components/function/endPoint/Refresh.jsx
@@ -5,11 +5,13 @@ export const Refresh = (refreshToken) => {
         refresh: refreshToken
     })
     .then(function (response) {
-        console.log(response.data.access)
+        
         console.log("Refresh!")
         return(
             window.localStorage.setItem("access", response.data.access),
-            window.localStorage.setItem("refresh", response.data.refresh)
+            window.localStorage.setItem("refresh", response.data.refresh),
+            console.log(response.data.access),
+            console.log(response.data.refresh)
         )
     })
     .catch(function (error) {
