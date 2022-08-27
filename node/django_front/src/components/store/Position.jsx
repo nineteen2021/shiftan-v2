2d4db46588f20021d5c948204d8e7699492b2070
KaedeNozawa <825noza@gmail.com>
2022-08-27 12:46:35 +0900

    ポジションを追加（エラー）

--- node/django_front/src/components/store/Position.jsx
+++ node/django_front/src/components/store/Position.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -11,63 +12,94 @@ import ColorPicker from '../function/ColorPicker';
 import TextField from '@mui/material/TextField';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { areArraysEqual } from '@mui/material/node_modules/@mui/base';
+import axios from 'axios';
 
 
 const rows = [];
 
-function AddPos() {
-    const input = document.getElementById("standard-basic");
-    rows.push(input.value);
-    console.log(rows)
-}
 
 function DelPos() {
-    if(select.current.length == 0) return;
-    const newRows = rows.filter(v => select.current.indexOf(v) == -1); 
-    setRows(newRows);
-    rows = newRows; 
+    axios.delete('/group', {data: {userId: 'xxxx'}})
+    .then(res => {console.log(res.data);})
 }
 
+function AddPos() {
+    axios.post('/group', {group_name: '三郎'})
+      .then(function (response) {
+        console.log(response.data);
+      });
+};
+
 export default function Position() {
+
+    const [groups, setGroups] = useState(null)
+    const [name, setName] = useState('');
+    
+    useEffect(() => {
+        axios
+        .get('http://localhost:8000/api/group/',{
+            headers: {
+                'Authorization': `JWT ${localStorage.getItem('access')}`, // ここを追加
+            }
+        })
+        .then(res=>{setGroups(res.data);
+                    console.log(res.data);})
+        .catch(err=>{console.log(err);});
+        }, []);
+
+    const handleNewName = (e) => {
+            setName(e.target.value)
+        }
+    
+    const createNewGroup = () => {
+        axios.post('https://localhost:8000/api/group/', {
+            
+            name: name,
+        })
+            .then(response => {
+                setGroups([...groups, response.data])
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+    
      return (
         <>
-        <Grid container>
-            <Grid item><Typography sx={{ mb:2, ml:2 ,mr:1}}><TextField sx={{minWidth: 230 }} id="standard-basic" label="追加したいポジションを入力" variant="standard" /></Typography></Grid>
-            <Grid item><Button variant="contained" sx={{ mt: 2 , mr:1}} onClick={AddPos}>追加</Button></Grid>
-            <Grid item><Button variant="contained" sx={{ mt: 2 , mr:1}} onClick={DelPos}>削除</Button></Grid>
-        </Grid>
-        <TableContainer component={Paper}>
-            <Table sx={{minWidth: 550 }} aria-label="caption table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align='left'>□</TableCell>
-                        <TableCell align='left'>色</TableCell>
-                        <TableCell align='left'>ポジション名</TableCell>
-                        <TableCell align="center"></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((rows,index) => (
-                        <TableRow>
-                            <TableCell>
-                                <Checkbox 
-                                disableSelectionOnClick
-                                onSelectionModelChange={(v) => select.current = v}/>
-                            </TableCell>
-                            <TableCell>
-                                <ColorPicker />
-                            </TableCell>
-                            <TableCell component="th" scope="row">{rows}</TableCell>
-                            <TableCell align="center">
-                                <Button variant="outlined" color="secondary" startIcon={<DeleteIcon />}>
-                                    消去
-                                </Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+            <Fragment>
+                <Grid container>
+                    <Grid item><Typography sx={{ mb:2, ml:2 ,mr:1}}><TextField sx={{minWidth: 230 }} id="standard-basic" label="追加したいポジションを入力" variant="standard" onChange={handleNewName}/></Typography></Grid>
+                    <Grid item><Button variant="contained" sx={{ mt: 2 , mr:1}} onClick={createNewGroup}>追加</Button></Grid>
+                    <Grid item><Button variant="contained" sx={{ mt: 2 , mr:1}} onClick={DelPos}>削除</Button></Grid>
+                </Grid>
+                <TableContainer component={Paper}>
+                    <Table sx={{minWidth: 550 }} aria-label="caption table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align='left'><Checkbox></Checkbox></TableCell>
+                                <TableCell align='left'>色</TableCell>
+                                <TableCell align='left'>ポジション名</TableCell>
+                                <TableCell align="center"></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {groups?.map(group => (
+                                <TableRow>
+                                    <TableCell>
+                                        <Checkbox></Checkbox>
+                                    </TableCell>
+                                    <TableCell>
+                                        {/* group.group_color */}
+                                        {<ColorPicker />}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row" >{group.group_name}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Fragment>
         </>
+        
     );
 }
