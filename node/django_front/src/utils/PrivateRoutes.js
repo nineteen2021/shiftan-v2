77518853996e6e3517bff7b88c6634fa1dff3072
koki-fore <furukunn@gmail.com>
2022-08-28 12:15:00 +0900

    privateRoutesうまくいっていない

--- node/django_front/src/utils/PrivateRoutes.js
+++ node/django_front/src/utils/PrivateRoutes.js
@@ -1,13 +1,45 @@
+import { getBottomNavigationUtilityClass } from '@mui/material';
+import axios from 'axios';
+import { useState } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
-const PrivateRoutes = () => {
+const PrivateRoutes = async() => {
     const auth = useAuth();
+    const [users, setUsers] = useState(null)
     console.log(auth);
+    let userMe
+    // const getUser = new Promise((resolved, rejected) => {
+    //     (() => {
+            // axios.get('http://localhost:8000/api-auth/users/me/',{
+            //     headers: {
+            //     'Authorization': `JWT ${localStorage.getItem('access')}`, // ここを追加
+            //     }
+            // })
+            // .then(res=>{
+            //     setUsers(res.data)
+            //     console.log(res.data)
+            //     userMe = res.data
+            // })
+    //         .catch(err=>{console.log(err)})
+    //     })();
+    // })
+
+    await axios.get('http://localhost:8000/api-auth/users/me/',{
+                headers: {
+                'Authorization': `JWT ${localStorage.getItem('access')}`, // ここを追加
+                }
+            })
+            .then(res=>{
+                setUsers(res.data)
+                console.log(res.data)
+                userMe = res.data
+            })
 
     console.log('Private route works!');
     return(
-        auth.accessToken != "undefined" ? <Outlet/> : <Navigate to='/login'/>
+        auth.accessToken != "undefined" ? <Outlet/> : <Navigate to='/login'/>,
+        userMe.is_manager === false ? <Outlet/> : <Navigate to='/*'/>
     )
 }
 export default PrivateRoutes
