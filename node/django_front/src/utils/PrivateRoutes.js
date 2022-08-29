9ab69fdb850a4a7db60389763692e41993c656db
koki-fore <furukunn@gmail.com>
2022-08-29 10:53:08 +0900

    PrivateRoutesをもとに戻した。

--- node/django_front/src/utils/PrivateRoutes.js
+++ node/django_front/src/utils/PrivateRoutes.js
@@ -4,42 +4,12 @@ import { useState } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
-const PrivateRoutes = async() => {
+const PrivateRoutes = () => {
     const auth = useAuth();
-    const [users, setUsers] = useState(null)
     console.log(auth);
-    let userMe
-    // const getUser = new Promise((resolved, rejected) => {
-    //     (() => {
-            // axios.get('http://localhost:8000/api-auth/users/me/',{
-            //     headers: {
-            //     'Authorization': `JWT ${localStorage.getItem('access')}`, // ここを追加
-            //     }
-            // })
-            // .then(res=>{
-            //     setUsers(res.data)
-            //     console.log(res.data)
-            //     userMe = res.data
-            // })
-    //         .catch(err=>{console.log(err)})
-    //     })();
-    // })
-
-    await axios.get('http://localhost:8000/api-auth/users/me/',{
-                headers: {
-                'Authorization': `JWT ${localStorage.getItem('access')}`, // ここを追加
-                }
-            })
-            .then(res=>{
-                setUsers(res.data)
-                console.log(res.data)
-                userMe = res.data
-            })
-
     console.log('Private route works!');
     return(
-        auth.accessToken != "undefined" ? <Outlet/> : <Navigate to='/login'/>,
-        userMe.is_manager === false ? <Outlet/> : <Navigate to='/*'/>
+        auth.accessToken != "undefined" ? <Outlet/> : <Navigate to='/login'/>
     )
 }
 export default PrivateRoutes
