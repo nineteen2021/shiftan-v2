f67e877257b516579c51d801b81baf4a929df2a8
yamato3010 <yama3to10yama3@gmail.com>
2022-09-13 15:51:39 +0900

    未使用コンポーネントのインポートを削除

--- node/django_front/src/components/store/Certification.jsx
+++ node/django_front/src/components/store/Certification.jsx
@@ -13,7 +13,6 @@ import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 
 function createData(name, applicationDate) {
   return { name, applicationDate};
