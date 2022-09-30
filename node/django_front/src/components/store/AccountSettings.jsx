f1a7ccf79ad1af0769f4a537ac975895c8fd9a08
DESKTOP-I5BEVNJ\jin_h <jin.codegym1217@gmail.com>
2022-09-30 14:18:54 +0900

    不要なコメントアウト削除

--- node/django_front/src/components/store/AccountSettings.jsx
+++ node/django_front/src/components/store/AccountSettings.jsx
@@ -63,11 +63,11 @@ export default function AccountSettings() {
         axios
         .get('http://localhost:8000/api-auth/users/me/',{
             headers: {
-                'Authorization': `JWT ${window.localStorage.getItem('access')}`, // ここを追加
+                'Authorization': `JWT ${window.localStorage.getItem('access')}`,
             }
         })
         .then(res=>{setUsers(res.data);
-                    //console.log(res.data);
+                    console.log(res.data);
                 })
         .catch(err=>{console.log(err);});
     }, []);
