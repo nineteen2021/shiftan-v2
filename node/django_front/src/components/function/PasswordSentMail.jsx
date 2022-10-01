e67510b751c302ca225dc8801e9207541c635da4
DESKTOP-I5BEVNJ\jin_h <jin.codegym1217@gmail.com>
2022-10-01 12:43:20 +0900

    footerの名前を英字に変更

--- node/django_front/src/components/function/PasswordSentMail.jsx
+++ node/django_front/src/components/function/PasswordSentMail.jsx
@@ -17,7 +17,7 @@ function Copyright(props) {
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        しふたん
+        Shiftan
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
